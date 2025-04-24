3860795e73f8d7e8c26d92c378c2ad61f675c732
Callum Lin <calluml1225@gmail.com>
2025-04-25 00:13:37 +0100

    technically not needed

--- plot.py
+++ plot.py
@@ -2,10 +2,17 @@ import matplotlib.pyplot as plt
 
 volumes = [i/100 for i in range(10, 100)]
 times = list(map(float, input("Enter the times: \n").split(",")))
-print(times, volumes)
+
+with open("output.csv", "w") as file:
+    file.write("Volume of water (l), Flight Time(s)\n")
+with open("output.csv", "a") as file:
+    for i, j in zip(volumes, times):
+        file.write(f"{i}, {j}\n")
+
+#print(times, volumes)
 
 plt.plot(volumes, times, marker="x")
-plt.ylabel("Time taken for rocket to land (s)")
+plt.ylabel("Flight Time (s)")
 plt.xlabel("Volume of water (l)")
 plt.grid(True)
 plt.show()
